7cb5394c5e8b379af56466a0c24a0e2fbd3e9ce3
Habib Talib <habib.talib@seekasia.com>
2017-05-11 02:14:29 +0800

    Modify hero countdown

--- resources/views/glayouts/hero.blade.php
+++ resources/views/glayouts/hero.blade.php
@@ -1,66 +1,28 @@
 <div class="hero-section has-background height-500px background-is-dark">
     <div class="slider">
         <div class="owl-carousel" data-owl-nav="0" data-owl-dots="1" data-owl-loop="1">
-            <div class="slide center">
-                <div class="coupon">
-                    <div class="wrapper">
-                        <div class="image">
-                            <figure class="circle">-59%</figure>
-                            <div class="bg-transfer"><img src="assets/img/items/15.jpg" alt=""></div>
-                        </div>
-                        <div class="description">
-                            <h1>Kursus Tadabbur Al-Quran: Ibrah Kisah Nabi Musa</h1>
-                            <figure class="label label-default">From RM60</figure>
-                            <figure class="location"><i class="fa fa-map-marker"></i>Interactive Dakwah Training (IDT) Centre</figure>
-                            <hr>
-                            <div class="count-down" data-countdown-year="2017" data-countdown-month="1" data-countdown-day="21"></div>
-                            <a href="/detail/1" class="btn btn-default btn-framed btn-rounded">View Detail</a>
-                        </div>
-                    </div>
-                </div>
-                <!--end coupon-->
-            </div>
-            <!--end slide-->
-            <div class="slide center">
-                <div class="coupon">
-                    <div class="wrapper">
-                        <div class="image">
-                            <figure class="circle">-20%</figure>
-                            <div class="bg-transfer"><img src="assets/img/items/5.jpg" alt=""></div>
-                        </div>
-                        <div class="description">
-                            <h1>PROGRAM LET US BE MUSLIMS: KELAS NEW MUSLIM</h1>
-                            <figure class="label label-default">From RM30</figure>
-                            <figure class="location"><i class="fa fa-map-marker"></i>Interactive Dakwah Training (IDT) Centre</figure>
-                            <hr>
-                            <div class="count-down" data-countdown-year="2017" data-countdown-month="6" data-countdown-day="21"></div>
-                            <a href="detail.html" class="btn btn-default btn-framed btn-rounded">View Detail</a>
+            @foreach( $recent as $item)
+                <div class="slide center">
+                    <div class="coupon">
+                        <div class="wrapper">
+                            <div class="image">
+                                <!--<figure class="circle">-59%</figure>-->
+                                <div class="bg-transfer"><img src="{{url('/').'/'.$item->gallery->first()['image']}}" alt=""></div>
+                            </div>
+                            <div class="description">
+                                <h1>{{$item['title']}}</h1>
+                                <figure class="label label-default">{{$item['fee']}}</figure>
+                                <figure class="location"><i class="fa fa-map-marker"></i>{{$item['address']}}</figure>
+                                <hr>
+                                <div class="count-down" data-countdown-year="{{Carbon\Carbon::parse($item->start_date)->year}}" data-countdown-month="{{Carbon\Carbon::parse($item->start_date)->month}}" data-countdown-day="{{Carbon\Carbon::parse($item->start_date)->day}}"></div>
+                                <a href="/detail/{{$item['id']}}" class="btn btn-default btn-framed btn-rounded">View Detail</a>
+                            </div>
                         </div>
                     </div>
+                    <!--end coupon-->
                 </div>
-                <!--end coupon-->
-            </div>
-            <!--end slide-->
-            <div class="slide center">
-                <div class="coupon">
-                    <div class="wrapper">
-                        <div class="image">
-                            <figure class="circle">-26%</figure>
-                            <div class="bg-transfer"><img src="assets/img/items/27.jpg" alt=""></div>
-                        </div>
-                        <div class="description">
-                            <h1>Weekend Trip to Venice</h1>
-                            <figure class="label label-default">From $130</figure>
-                            <figure class="location"><i class="fa fa-map-marker"></i>Venice</figure>
-                            <hr>
-                            <div class="count-down" data-countdown-year="2017" data-countdown-month="6" data-countdown-day="21"></div>
-                            <a href="detail.html" class="btn btn-default btn-framed btn-rounded">View Detail</a>
-                        </div>
-                    </div>
-                </div>
-                <!--end coupon-->
-            </div>
-            <!--end slide-->
+                <!--end slide-->
+            @endforeach
         </div>
         <!--end owl-carousel-->
     </div>
